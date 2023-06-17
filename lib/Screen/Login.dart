2c404a6adb87f6c3b5f78308eec69fe1f72f0b47
Eddy Nathansyah <nathsyahed@gmail.com>
2023-06-17 18:31:38 +0700

    Testing Login

--- lib/Screen/Login.dart
+++ lib/Screen/Login.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
