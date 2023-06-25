e5e67030c47dba507b8d8614e3183a061321bf37
Adyatma Ruliff Brahmantyo_535210047 <adyatmaruliff@gmail.com>
2023-06-25 21:30:46 +0700

    util color

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(35, 40, 48, 1);
+const buttonColor = Color.fromRGBO(58, 62, 70, 1);
+const fontColor = Color.fromRGBO(238, 238, 238, 1);
+const hyperLinkColor = Color.fromRGBO(90, 203, 255, 1);
