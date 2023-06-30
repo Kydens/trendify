2b28e7040abc43650d6307653aa0ce4379eeb9c6
Cloovyy <vannessv11@gmail.com>
2023-06-30 20:15:11 +0700

    update MobileScreenLayout

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:trendify_clone/utils/color.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('this is mobile'),
+    return Scaffold(
+      backgroundColor:
+          Colors.transparent, // Mengubah latar belakang menjadi transparan
+      body: Center(),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: backgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? activeColor : nonActiveColor,
+            ),
+            label: '',
+            backgroundColor: buttonColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? activeColor : nonActiveColor,
+            ),
+            label: '',
+            backgroundColor: buttonColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? activeColor : nonActiveColor,
+            ),
+            label: '',
+            backgroundColor: buttonColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? activeColor : nonActiveColor,
+            ),
+            label: '',
+            backgroundColor: buttonColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? activeColor : nonActiveColor,
+            ),
+            label: '',
+            backgroundColor: buttonColor,
+          ),
+        ],
       ),
     );
   }
